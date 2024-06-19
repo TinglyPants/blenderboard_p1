5bf61c26e7cccab82a4b0c64ce0ef20edabe8597
TinglyPants <chalkytangfastics@gmail.com>
2024-06-19 11:54:35 +0100

    Added NavBar with components. This is a big commit, so check the individual files during writeup!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Logo from "./components/structural/Logo";
 import NavBar from "./components/structural/NavBar";
+import Profile from "./components/structural/Profile";
+import SearchBar from "./components/structural/SearchBar";
 
 export default function App() {
   return (
     <div className="bg-dark w-screen h-screen flex flex-col font-poppins"> {/* Whole Screen Wrapper Div*/}
-      <NavBar></NavBar>
+      <NavBar>
+        <Logo/>
+        <SearchBar />
+        <Profile 
+        username="ExampleUsername"
+        email="Us3rname_wowie_woah@example.com"
+        imageURL="https://images.pexels.com/photos/982300/pexels-photo-982300.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+      </NavBar>
     </div>
   );
 }
