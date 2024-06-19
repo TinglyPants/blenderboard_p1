6daafa84e0f6662ab3bbbc03f243db69dd9c451e
TinglyPants <chalkytangfastics@gmail.com>
2024-06-19 11:05:01 +0100

    Added SvgIcon helper component

---
+++ src/components/general/SvgIcon.js
@@ -0,0 +1,11 @@
+export default function SvgIcon({ path, name, onClick }){
+    return<svg
+            className="w-8 h-8 stroke-white mx-2 group-hover:stroke-highlight hover:stroke-highlight"
+            viewBox="0 0 24 24"
+            fill="none"
+            title={name}
+            onClick={onClick}
+        >
+            <path d={path} />
+        </svg>
+}
