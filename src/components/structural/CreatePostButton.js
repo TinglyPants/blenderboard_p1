9632d59e3d8fdf91c88a7b040452acbf6c7af874
TinglyPants <chalkytangfastics@gmail.com>
2024-06-20 09:15:50 +0100

    Sid bar buttons now redirect properly.

--- src/components/structural/CreatePostButton.js
+++ src/components/structural/CreatePostButton.js
@@ -1,6 +1,16 @@
-export default function CreatePostButton({ onClick }) {
+import { useNavigate } from "react-router-dom";
+
+export default function CreatePostButton() {
+    const navigate = useNavigate();
+    const handleRedirect = () => {
+        navigate("/create-post");
+    };
+
     return (
-        <button className="bg-highlight rounded-full mt-[2rem] py-[0.75rem] px-[1.25rem] text-lg">
+        <button
+            className="bg-highlight rounded-full mt-[2rem] py-[0.75rem] px-[1.25rem] text-lg"
+            onClick={handleRedirect}
+        >
             Create Post
         </button>
     );
--- src/components/structural/SideBarItem.js
+++ src/components/structural/SideBarItem.js
@@ -1,10 +1,16 @@
 import SvgIcon from "../general/SvgIcon";
+import { useNavigate } from "react-router-dom";
 
-export default function SideBarItem({ name, path, onClick }) {
+export default function SideBarItem({ name, path, url, onClick }) {
+    const navigate = useNavigate();
+    const handleRedirect = () => {
+        navigate(url);
+    };
+
     return (
         <div
             className="grid grid-cols-3 mx-[2rem] w-[10rem] cursor-pointer mt-[1rem] group"
-            onClick={onClick}
+            onClick={onClick === undefined ? handleRedirect : onClick}
         >
             {" "}
             {/* Wrapper for side bar item, allows better spacing. */}
