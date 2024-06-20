db9902bef91a82703dbaddd0cc33415438089f20
TinglyPants <chalkytangfastics@gmail.com>
2024-06-20 09:21:37 +0100

    Added redirect functionality to profile component

--- src/components/structural/Profile.js
+++ src/components/structural/Profile.js
@@ -1,6 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Profile({ username, email, imageURL }) {
+    const navigate = useNavigate();
+    const handleRedirect = () => {
+        navigate("/account");
+    };
+
     return (
-        <div className="align-bottom cursor-pointer mr-[4rem] flex flex-row">
+        <div
+            className="align-bottom cursor-pointer mr-[4rem] flex flex-row"
+            onClick={handleRedirect}
+        >
             {" "}
             {/* Wrapping Div */}
             <div className="h-[3rem] flex flex-col mr-[1rem] text-right">
