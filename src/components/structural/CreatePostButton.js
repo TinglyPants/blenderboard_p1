a2b53931c0922681274855ecf62eff44a05fdbb5
TinglyPants <chalkytangfastics@gmail.com>
2024-06-20 08:49:16 +0100

    Added create post button.

---
+++ src/components/structural/CreatePostButton.js
@@ -0,0 +1,7 @@
+export default function CreatePostButton({ onClick }) {
+    return (
+        <button className="bg-highlight rounded-full mt-[2rem] py-[0.75rem] px-[1.25rem] text-lg">
+            Create Post
+        </button>
+    );
+}
