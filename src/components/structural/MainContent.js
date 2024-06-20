3380812f63ece16f0765fc5d1bd6e9399834c8b1
TinglyPants <chalkytangfastics@gmail.com>
2024-06-20 09:03:22 +0100

    Added main content wrapper

---
+++ src/components/structural/MainContent.js
@@ -0,0 +1,3 @@
+export default function MainContent({ children }) {
+    return <div className="w-full h-full text-white">{children}</div>; // This is a semantic component, it has no real functionality
+}
