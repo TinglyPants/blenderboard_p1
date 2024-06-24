57c644354659db2bff586a79d7de8a012318077f
TinglyPants <chalkytangfastics@gmail.com>
2024-06-24 10:00:59 +0100

    Modified SVG component to allow better scaling. Needs to be wrapped inside of a div to work.

--- src/components/general/SvgIcon.js
+++ src/components/general/SvgIcon.js
@@ -1,7 +1,7 @@
 export default function SvgIcon({ path, name, onClick }) {
     return (
         <svg
-            className="w-8 h-8 stroke-white mx-2 group-hover:stroke-highlight hover:stroke-highlight"
+            className="w-max h-max stroke-white mx-2 group-hover:stroke-highlight hover:stroke-highlight"
             viewBox="0 0 24 24"
             fill="none"
             title={name}
